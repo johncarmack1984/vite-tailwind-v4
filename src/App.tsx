f8feaa1184fa325dd4efc2b2c09a5aed89271d7a
John Carmack <johncarmack@me.com>
2024-12-01 13:40:02 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex bg-background text-foreground flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
